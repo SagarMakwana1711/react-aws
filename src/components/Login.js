56d5755a38ea0fe8aeb2171211c87d90f2fa3bab
Sagar Makwana <sagar.makwana@tatvasoft.com>
2025-08-29 16:48:14 +0530

    Check cache

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const TITLE = 'Welcome back for cache';
+const TITLE = 'Welcome back for cache invalidation';
 const BTN_TEXT = 'Sign in';
 const USER_TEXT = 'Username';
 const PASS_TEXT = 'Password';
